927bee821f59e35baca4d96b89e2a065e843da5c
Ruslan Kladko <rkladko@gmail.com>
2025-07-22 10:30:26 +0200

    ready for romance

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/mmcdole/gofeed"
+)
+
+type Feeds struct {
+	Version string  `json:"version"`
+	Items   []*Feed `json:"items"`
+}
+
+type Feed struct {
+	Type    string `json:"type"`
+	Hash    string `json:"hash"`
+	Url     string `json:"url"`
+	Updated string `json:"updated"`
+}
+
+func main() {
+
+	file, err := os.Open("feeds.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var feeds Feeds
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&feeds); err != nil {
+		panic(err)
+	}
+	file.Close()
+
+	for _, f := range feeds.Items {
+
+		fmt.Printf("Hash: %s, Updated: %s, URL: %s\n", f.Hash, f.Updated, f.Url)
+
+		fp := gofeed.NewParser()
+		feed, err := fp.ParseURL(f.Url)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if feed.Updated != f.Updated {
+			fmt.Println("Feeds updated", len(feed.Items))
+			f.Updated = feed.Updated
+			for _, item := range feed.Items {
+				fmt.Printf("Title: %s, Date: %s, GUID: %s\n", firstNRunes(item.Title, 64), item.Updated, item.GUID)
+			}
+		} else {
+			fmt.Println("No new items")
+		}
+
+	}
+
+	// save json
+	file, err = os.Create("feeds.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(feeds); err != nil {
+		panic(err)
+	}
+
+}
+
+func firstNRunes(s string, n int) string {
+	runes := []rune(s)
+	if n >= len(runes) {
+		return s
+	}
+	return string(runes[:n])
+}
